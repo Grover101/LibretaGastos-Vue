d647c0bcf2726e7951cd140b60a4c16ff3834247
Grover101 <limachigrover09@gmail.com>
2020-04-30 17:31:43 -0400

    Importando estilos de bootstrap y font-awesome

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
+import '../node_modules/font-awesome/css/font-awesome.css'
 Vue.config.productionTip = false
 
 new Vue({
