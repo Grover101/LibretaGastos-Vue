5977130065fde78369cd4dd036e7caadace6895c
Grover101 <limachigrover09@gmail.com>
2020-05-01 23:50:22 -0400

    Agregue el form para agregar los gastos

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,70 @@
 <template>
   <div v-if="logon" id="app" class="container">
     <!-- Contenido de las listas de gasto -->
-    <h1>hola mundo</h1>
+    <div class="row text-primary">
+      <div class="col-10">
+        <h1>Lista de Gastos</h1>
+      </div>
+      <div class="col-2">
+        <span
+          v-bind:id="'agregar'"
+          v-bind:class="['mr-1', 'btn', 'btn-primary', boton]"
+          style="font-size: 10px; margin-top: 15px"
+          v-on:click="butonPlus($event)"
+        ></span>
+      </div>
+    </div>
+    <div
+      v-if="despliegue"
+      key="despliegue"
+      class="container border rounded text-primary lead p-2 mt-3"
+    >
+      <div class="row justify-content-md-center">
+        <div class="col-3"></div>
+        <div class="col-3">Nombre del gasto</div>
+        <div class="col-3">
+          <input v-model="nombreGasto" class="text-primary" type="text" />
+        </div>
+        <div class="col-3"></div>
+      </div>
+      <div class="row justify-content-md-center my-2">
+        <div class="col-3"></div>
+        <div class="col-3">Monto del gasto</div>
+        <div class="col-3">
+          <input v-model="montoGasto" class="text-primary" type="text" />
+        </div>
+        <div class="col-3"></div>
+      </div>
+      <div class="row justify-content-md-center my-2">
+        <div class="col-3"></div>
+        <div class="col-3">Tipo de gasto</div>
+        <div class="col-3">
+          <input v-model="tipoGasto" class="text-primary" type="text" />
+        </div>
+        <div class="col-3"></div>
+      </div>
+      <div class="row justify-content-md-center my-2">
+        <div v-if="actualizar" class="col-12">
+          <div id="actualizar" class="btn btn-primary" v-on:click="manejarClick($event)">Actualizar</div>
+        </div>
+        <div v-else class="col-12">
+          <div id="agregar" class="btn btn-primary" v-on:click="manejarClick($event)">Agregar</div>
+        </div>
+      </div>
+    </div>
+    <!-- filtros -->
+    <div class="container border rounded text-primary mt-4">
+      <div class="row lead border rounded font-weight-bold">
+        <div class="col-3">Nombre del gasto</div>
+        <div class="col-3">Monto del gasto</div>
+        <div class="col-3">Tipo de gasto</div>
+        <div class="col-3">Accion</div>
+      </div>
+      <!-- lista de los gastos -->
+    </div>
   </div>
   <div v-else>
+    <!-- Contenido de login -->
     <loginForm v-bind:firebase="firebase" v-on:ingresoCorrecto="ingresoCorrecto($event)"></loginForm>
   </div>
 </template>
@@ -17,15 +78,48 @@ export default {
   name: "app",
   data: function() {
     return {
+      boton: "fa fa-plus",
       gastos: [],
+      filtrados: [],
       coleccion: {},
       logon: false,
       firebase: "",
       idUsuario: "",
-      db: ""
+      db: "",
+      actualizar: false,
+      despliegue: false
     };
   },
   methods: {
+    butonPlus: function(event) {
+      if (event.target.id === "agregar") {
+        this.boton = "fa fa-minus";
+        document.getElementById("agregar").setAttribute("id", "actualizar");
+        this.despliegue = true;
+      } else if (event.target.id === "actualizar") {
+        this.boton = "fa fa-plus";
+        document.getElementById("actualizar").setAttribute("id", "agregar");
+        this.despliegue = false;
+        this.actualizar = false;
+      }
+    },
+    manejarClick: function(evento) {
+      if (evento.target.id === "agregar") {
+        // Se agregara los nuevos gastos
+
+        this.despliegue = false;
+        this.boton = "fa fa-plus";
+        document.getElementById("actualizar").setAttribute("id", "agregar");
+      }
+      if (evento.target.id === "actualizar") {
+        // Actualizando los gastos
+
+        this.despliegue = false;
+        this.actualizar = false;
+        this.boton = "fa fa-plus";
+        document.getElementById("actualizar").setAttribute("id", "agregar");
+      }
+    },
     ingresoCorrecto: function(usuario) {
       console.log("User: " + usuario);
       this.idUsuario = usuario;
@@ -39,6 +133,7 @@ export default {
             nombre: gasto.data().NombreGasto,
             tipo: gasto.data().TipoGasto
           });
+          this.totalGasto += parseFloat(gasto.data().MontoGasto);
         });
       });
     }
@@ -49,10 +144,10 @@ export default {
   beforeMount: function() {
     var config = {
       apiKey: " AIzaSyCMnEqx827ZNmelgPgL0gC81DhSIXBLs6s",
-      authDomain: "vue-practica-f8ecc.firebaseapp.com",
+      authDomain: "vue-practica-f8ec.firebaseapp.com",
       databaseURL: "https://vue-practica-f8ecc.firebaseio.com",
       projectId: "vue-practica-f8ecc",
-      storageBucket: "vue-practica-f8ecc.appspot.com",
+      storageBucket: "vue-practica-f8ecappspot.com",
       messagingSenderId: "310273136041"
     };
     firebase.initializeApp(config);
