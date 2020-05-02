c4f2766d8394852ab3da736c0f6952c1f09e10bd
Grover101 <limachigrover09@gmail.com>
2020-05-02 13:20:32 -0400

    Cree el componente filtros y su funcionamiento, ya esta terminado

---
+++ src/components/FiltrosComponente.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <div class="row lead my-1">
+      <div class="col-3">{{gasto.nombre}}</div>
+      <div class="col-3">{{gasto.monto}}</div>
+      <div class="col-3">{{gasto.tipo}}</div>
+      <div class="col-1">
+        <div
+          id="editar"
+          class="btn btn-primary"
+          v-on:click="editar($event,{
+            indice:indice, 
+            id:id, 
+            nombre: gasto.nombre,
+            monto: gasto.monto,
+            tipo: gasto.tipo,
+            mostrar: true
+          })"
+        >editar</div>
+      </div>
+      <div class="col-2">
+        <div
+          id="eliminar"
+          class="btn btn-primary"
+          v-on:click="manejarClick($event,{indice:indice, id:id, monto: gasto.monto})"
+        >Eliminar</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "filtros",
+  props: ["gasto", "id", "indice"],
+  data: function() {
+    return {
+      test: "test",
+      monstrar: false
+    };
+  },
+  methods: {
+    manejarClick: function(evento, gastoID) {
+      if (evento.target.id === "eliminar") {
+        this.$emit("eliminarGasto", gastoID);
+      }
+    },
+    editar: function(event, cambios) {
+      this.$emit("editarGasto", cambios);
+      //   this.monstrar = true;
+    },
+    agregarTarea: function(event) {
+      console.log(event.mostrar);
+      this.$emit("editarGasto", event);
+      if (!event.mostrar) this.monstrar = false;
+    }
+  }
+};
+</script>
+
+<style>
+</style>
